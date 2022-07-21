7cbf9e8e0d540e7cad5a2314e71793a1697f596e
Jeeeyoungkim <ye_deun@naver.com>
2022-07-21 23:06:58 +0900

    commit

---
+++ React/src/Slogun.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    SlogunSection,
+    SlogunBig,
+    SlogunSmall,
+  } from './styledComponent';
+
+function Slogan() {
+  return (
+    <SlogunSection>
+    <SlogunBig>HACK YOUR LIFE</SlogunBig>
+    <SlogunSmall>내 아이디어를 내 손으로 실현한다.</SlogunSmall>
+  </SlogunSection>
+  )
+}
+
+export default Slogan
